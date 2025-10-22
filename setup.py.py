01b2eb34d66cfd9173ca7ca24e091a6b2963f460
Sela Shapira <157536183+Selashap@users.noreply.github.com>
2025-10-22 10:43:02 +0100

    creating a package

---
+++ setup.py.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'My_work'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='SS4568@live.mdx.ac.uk',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        	'Robot_movement = My_work.Robot_move:main' 
+        ],
+    },
+)
